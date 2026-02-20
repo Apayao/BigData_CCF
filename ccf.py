8750f320ed0b608c2ad47ce40e9969c30f08142d
Apayao <64597087+Apayao@users.noreply.github.com>
2026-02-20 15:42:15 +0100

    main CCF + graph gen

---
+++ ccf.py
@@ -0,0 +1,123 @@
+
+
+
+def ccf_dedup(rdd):
+    mapped_rdd = rdd.map(lambda edge: (edge, None))
+
+    # Shuffle & Sort va regrouper toutes les keys identiques
+    # On utilise reduceByKey pour ne garder qu'une seule occurrence.
+    reduced_rdd = mapped_rdd.reduceByKey(lambda v1, v2: v1)
+    final_rdd = reduced_rdd.map(lambda kv: kv[0])
+
+    # On pourra éventuellement comparer cette implémentation avec le distinct() natif en Spark par la suite
+
+    return final_rdd
+
+
+def ccf_iterate_vanilla(rdd, new_pair_accum):
+
+    bothDirections = rdd.flatMap(lambda x: [(x[0], x[1]), (x[1], x[0])])
+
+    def process_vanilla(key_values):
+        key, values = key_values
+        val_list = list(values)  # la conversion en list() force le chargement en RAM de toutes les valeurs
+        min_val = min(val_list)
+
+        emitted = []
+        if min_val < key:
+            emitted.append((key, min_val))
+            for v in val_list:
+                if v != min_val:
+                    new_pair_accum.add(1)
+                    emitted.append((v, min_val))
+        return emitted
+
+    return bothDirections.groupByKey().flatMap(process_vanilla)
+
+
+def ccf_iterate_SecondSort_naive(rdd, new_pair_accum):
+    adj = rdd.flatMap(lambda x: [(x[0], x[1]), (x[1], x[0])])
+
+    def process_ss_naive(key_values):
+        key, values = key_values
+
+        # Pourquoi est-ce qu'on a ici une méthode dite naïve ?
+        # On voudrait recopier exactement l'algorithme du papier, en ayant une liste triée dont on choisirait la première valeur
+        # Sauf qu'en faisant ça, on stocke les valeurs en mémoire, et on demande à les trier ensuite
+        # Finalement, on a une complexité spatiale O(n) en RAM + opération de tri lourde infligée au CPU
+        sorted_vals = sorted(list(values))
+        min_val = sorted_vals[0]
+
+        emitted = []
+        if min_val < key:
+            emitted.append((key, min_val))
+            for v in sorted_vals:
+                if v != min_val:
+                    new_pair_accum.add(1)
+                    emitted.append((v, min_val))
+        return emitted
+
+    return adj.groupByKey().flatMap(process_ss_naive)
+
+
+def ccf_iterate_optimised(rdd, new_pair_accum):
+
+    bothDirections = rdd.flatMap(lambda x: [(x[0], x[1]), (x[1], x[0])])
+    mins = bothDirections.reduceByKey(lambda v1, v2: v1 if v1 < v2 else v2) #on calcule les minima locaux à chaque fois, sans surcharger la RAM
+    # on préfère utiliser reduceByKey ici (pour la raison énoncée juste avant), plutôt que groupByKey.
+    # En effet, groupByKey shuffle toutes les données pour que les mêmes valeurs associées à une même clé se retrouvent sur une même partition.
+    # Or, ici, on n'a pas besoin de garder la liste d'adjacence complète pour chaque sommet, uniquement le sommet de valeur minimale
+    # On choisit donc reduceByKey, pour calculer le minimum localement sur chaque partition avant de transférer quoi que ce soit sur le réseau
+
+
+    mins_filtered = mins.filter(lambda x: x[1] < x[0])
+    emit_1 = mins_filtered
+
+    # ÉMISSION 2 : (v, min_val)
+    # On fait une jointure entre l'adjacence d'origine (key, v) et les minimums (key, min_val)
+    # Résultat du join : (key, (v, min_val))
+    joined = bothDirections.join(mins_filtered)
+
+    def process_joined(item):
+        key, (v, min_val) = item
+        if v != min_val:
+            new_pair_accum.add(1)
+            return [(v, min_val)]
+        return []
+
+    emit_2 = joined.flatMap(process_joined)
+    return emit_1.union(emit_2)
+
+
+def run_ccf(sc, rdd, method="vanilla"):
+
+    iteration = 1
+
+    while True:
+        new_pair_accum = sc.accumulator(0)
+
+        if method == "vanilla":
+            iterated_rdd = ccf_iterate_vanilla(rdd, new_pair_accum)
+        elif method == "sec_sort_naive":
+            iterated_rdd = ccf_iterate_SecondSort_naive(rdd, new_pair_accum)
+        elif method == "optimised":
+            iterated_rdd = ccf_iterate_optimised(rdd, new_pair_accum)
+        else:
+            raise ValueError("Méthode inconnue. Choisissez: vanilla, sec_sort_naive, ou optimised")
+
+        rdd = ccf_dedup(iterated_rdd) # Rappel: il faudra comparer avec distinct
+
+        # Spark utilise la lazy evaluation: on doit forcer l'exécution avec un count()
+        # pour que l'accumulateur se mette à jour avant la condition d'arrêt.
+        rdd.cache()  # Mise en cache pour ne pas recalculer l'arbre à l'itération suivante
+        rdd.count()
+
+        new_pairs = new_pair_accum.value
+        print(f"[{method}] Iteration {iteration}: {new_pairs} new pairs.")
+
+        if new_pairs == 0:
+            break
+
+        iteration += 1
+
+    return rdd, iteration
---
+++ graph_data.py
@@ -0,0 +1,60 @@
+import networkx as nx
+import os
+
+
+# On veut générer des graphes de tailles croissantes.
+# Sauf qu'on ne peut pas les générer complètement au hasard, au risque de perdre l'interprétabilité
+# Si les graphes ne ressemblent pas aux données réelles (pas de liens, peu de sommets), l'évaluation en sera faussée
+
+def generate_small_world(num_nodes, num_edges, output_file_name=None):
+
+    # On utilise le modèle de Barabási-Albert
+    m = max(1, num_edges // num_nodes)
+
+    G = nx.barabasi_albert_graph(num_nodes, m)
+
+    if output_file_name is None:
+        output_file_name = f"SmallWorld_{num_nodes}_{num_edges}"
+
+    output_path = f"{output_file_name}.txt"
+
+    with open(output_path, 'w') as f:
+        for u, v in G.edges():
+            f.write(f"{u}\t{v}\n")
+    print(f"Small world graph was saved here : {output_path}")
+
+
+# On ne teste pas qu'avec des graphes sensiblement similaires à des données réelles
+# On génère également un cas pathologique. Selon l'article :
+
+# "Worst case scenario for the number of necessary iterations
+# is d+1 where d is the diameter of the network. The worst
+# case happens when the min node in the largest connected
+# component is an end-point of the largest shortest-path."
+
+# Notre idée ici est donc d'évaluer l'algo avec un graphe au diamètre élevé
+
+def generate_line_graph(num_nodes, output_file_name=None):
+
+    if output_file_name is None:
+        output_file_name = f"LineGraph_{num_nodes}"
+
+    output_path = f"{output_file_name}.txt"
+    with open(output_path, 'w') as f:
+        for i in range(1, num_nodes):
+            f.write(f"{i}\t{i + 1}\n")
+
+    print(f"Line graph was saved here: {output_path}")
+
+
+def load_graph_to_rdd(sc, file_path):
+
+    raw_rdd = sc.textFile(file_path)
+
+    filtered_rdd = raw_rdd.filter(lambda line: len(line.strip()) > 0 and not line.startswith('#'))
+
+    def parse_line(line):
+        edge = line.split()
+        return int(edge[0]), int(edge[1])
+
+    return filtered_rdd.map(parse_line)
